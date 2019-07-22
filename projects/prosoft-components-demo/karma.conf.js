5fa05367ec76ce0d944191856e38622416e762af
Benedikt Langer <benedikt.langer@prosoft.net>
2019-07-22 13:53:02 +0200

    refactor(demo): moved demo app to the projects folder

--- projects/prosoft-components-demo/karma.conf.js
+++ projects/prosoft-components-demo/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/prosoft-components-demo'),
+      dir: require('path').join(__dirname, '../../coverage/prosoft-components-demo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
